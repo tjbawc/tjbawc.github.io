43e666c0a98d07ede73430f864d3d794f33b2f9f
Hyper-caffeinated <156276098+Hyper-caffeinated@users.noreply.github.com>
2025-11-28 17:57:29 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   basePath: "/nextjs-blog",
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
