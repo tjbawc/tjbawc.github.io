706409fa43e72f04bcc8993c9a975fb55d1f31dc
Hyper-caffeinated <156276098+Hyper-caffeinated@users.noreply.github.com>
2026-01-01 19:23:51 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   basePath: "/nextjs-blog",
-  assetPrefix: "/nextjs-blog", // test: should be isProd ? '/<repository-name>' : '',
+  assetPrefix: "/nextjs-blog",
   output: "export",
   reactStrictMode: true,
   images: {
