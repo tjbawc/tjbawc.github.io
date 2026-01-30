d4dcd750a28e95876e2991987f7fb60def0dc805
Hyper-caffeinated <156276098+Hyper-caffeinated@users.noreply.github.com>
2026-01-29 19:31:52 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/nextjs-blog",
-  assetPrefix: "/nextjs-blog",
   output: "export",
   reactStrictMode: true,
   images: {
